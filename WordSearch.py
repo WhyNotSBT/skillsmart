58404f654e879591de8dc7f773b03608e8a30c22
SVMILLER <whynotsavagemiller@gmail.com>
2020-09-01 00:13:11 +0300

    add stack

--- WordSearch.py
+++ WordSearch.py
@@ -1,26 +1,40 @@
 def WordSearch(Len:int,s:str,subs:str):
-	i = 1
+	i = 0
 	Index = []
 	Check_Word = []
 	A = s.split()
-	while i < len(A):
-		line = A[i - 1]
-		if len(line) > Len:
-			line_0 = line[:Len]
-			Index.append(line_0)
-			line = line[Len:]
-		while (len(line) + len(A[i]) + 1) <= Len:
-			line = (line + " " + A[i])
-			if i < len(A) - 1:
-				i += 1
-		Index.append(line)
-		i += 1
-	if (i - 1 == len(A)-1) and not A[-1] in Index[-1]:
-		Index.append(A[-1])
+	N = len(A) - 1
+	flag = 0
+	for i in range(N//2 +1):
+		A[i],A[N-i] = A[N-i],A[i]
+	while len(A) >= 0:
+		if flag == 0:
+			word = A.pop()
+		else:
+			word = word1
+		while len(word) >= Len:
+			Index.append(word[:Len])
+			word = word[Len:]
+		if len(A) > 0:
+			word1 = A.pop()
+			flag = 1
+		else:
+			Index.append(word)
+			break
+		while (len(word) + 1 + len(word1)) <= Len:
+			word = (word + " " + word1)
+			if len(A) > 0:
+				word1 = A.pop()
+				flag = 1
+			else:
+				Index.append(word)
+				break
+		Index.append(word)
 	Check_Word = [0] * (len(Index))
 	for s in range(len(Index)):
 		In_Line = Index[s].split()
 		for k in In_Line:
 			if subs == k:
-				Check_Word[s] = 1
+				Check_Word[s] = 1		
 	return Check_Word
+
