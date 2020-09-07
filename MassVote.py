7ad0da4c80cabf1be9afcb0a6b9d65cec7408210
SVMILLER <whynotsavagemiller@gmail.com>
2020-09-07 14:49:33 +0300

    add Twelve task

---
+++ MassVote.py
@@ -0,0 +1,22 @@
+def MassVote(N,Vote):
+	case_3 = "no winner"
+	Max = 0
+	Sum = 0
+	Index = []
+	for i in range(N):
+		if Vote[i] > Max:
+			Max = Vote[i]
+		Sum += Vote[i]
+	for i in range(N):
+		if Vote[i] == Max:
+			Index.append(i)
+	if len(Index) > 1:
+		return case_3
+	Percent = (Max * 100) / Sum
+	Percent = "{0:.3f}".format(Percent)
+	if float(Percent) > 50:
+		case_1 = "majority winner " + str(Index[0] + 1)
+		return case_1
+	else:
+		case_2 = "majority winner " + str(Index[0] + 1)
+		return case_2
