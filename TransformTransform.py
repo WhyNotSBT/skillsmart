e833eda8d1f53de9dcf8cdfe9788544fa54a230e
SVMILLER <61921211+WhyNotSBT@users.noreply.github.com>
2020-09-27 18:37:59 +0300

    Update TransformTransform.py
    done

--- TransformTransform.py
+++ TransformTransform.py
@@ -1,17 +1,12 @@
 def S(A:list):
 	B = []
-	j = 0
-	for i in A:
-		while True:
+	for i in range(len(A)):
+		for j in range(len(A) - i):
 			k = i + j
 			B.append(max(A[j:k + 1]))
-			if j < (len(A) - i - 1):
-				j += 1
-			else:
-				break
 	return B
 
 def TransformTransform(A:list, N:int):
-	if (sum(S(S(A))) % 2) == 0:
+	if sum(S(S(A))) % 2 == 0:
 		return True
 	return False
