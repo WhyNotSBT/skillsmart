dfe7329faece494bc9b14a07015ac9bbd8b29c29
SVMILLER <whynotsavagemiller@gmail.com>
2020-09-28 23:12:27 +0300

    add 27th task

---
+++ Football.py
@@ -0,0 +1,19 @@
+def Football(F:list, N:int):
+	F_sort = sorted(F)
+	print(F)
+	print(F_sort)
+	Not_sort = []
+	A = []
+	for i in range(N):
+		if F[i] != F_sort[i]:
+			A.append(i)
+	if len(A) == 2:
+		F[A[0]],F[A[1]] = F[A[1]],F[A[0]]
+		if F == F_sort:
+			return True
+		else:
+			return False
+	F = F[:A[0]] + F[A[-1]:A[0] - 1:-1] + F[A[-1] + 1:]
+	if F == F_sort:
+		return True
+	return False
