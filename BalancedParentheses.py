7a2987e5b05b1a6a3849a45518394a5baec2fc99
SVMILLER <61921211+WhyNotSBT@users.noreply.github.com>
2020-09-28 21:21:12 +0300

    Update BalancedParentheses.py
    fix

--- BalancedParentheses.py
+++ BalancedParentheses.py
@@ -1,17 +1,13 @@
-def perm(n:int, s:str, op:int, cl:int):
-	global A
+def perm(n:int, s:str, op:int, cl:int, A):
 	if op + cl == n * 2:
 		A.append(s)
 	if op < n :
-		perm (n, s + '(', op + 1, cl)
+		perm (n, s + '(', op + 1, cl, A)
 	if op - cl > 0:
-		perm (n, s + ')', op, cl + 1)
-
+		perm (n, s + ')', op, cl + 1, A)
+	return A
 
 def BalancedParentheses(N):
-	global A
-	perm(N, s='', op=0, cl=0)
+	A = perm(N, s='', op=0, cl=0, A=[])
 	A = ' '.join(A)
-	return(A)
-
-A = []
+	return A
